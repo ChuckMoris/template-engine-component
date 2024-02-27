ba2aed813eca083b5b0a18a0cf8c90069cb6e869
ChuckMoris <161121235+ChuckMoris@users.noreply.github.com>
2024-02-27 18:52:59 +0400

    Add files via upload

--- src/TemplateRenderer.tsx
+++ src/TemplateRenderer.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import Icon from './Icon';
+﻿import React from 'react';
 
-interface TemplateProps {
+type IconConfig = Record<string, React.ReactNode>;
+type UtilConfig = Record<string, () => string>;
+
+type TemplateProps = {
     template: string;
-}
+    icons: IconConfig;
+    utils: UtilConfig;
+};
 
-const TemplateRenderer: React.FC<TemplateProps> = ({ template }) => {
+const Template: React.FC<TemplateProps> = ({ template, icons, utils }) => {
+    const handleClickTime = () => {
+        const time = utils['current_datetime']();
+        alert(`Current time is ${time}`);
+    };
+
+    const handleClickLanguage = () => {
+        const language = utils['language']();
+        alert(`Selected language is ${language}`);
+    };
+
     const renderTemplate = () => {
-        const parts = template.split(/{([^{}]+)}/g);
+        const parts = template.split(/({(?:icon|utils):[^{}]*})/);
         return parts.map((part, index) => {
-            if (index % 2 === 0) {
-                return <span key={index}>{part}</span>;
-            } else {
-                const [type, arg] = part.split(':');
-                switch (type) {
-                    case 'icon':
-                        return <Icon key={index} name={arg} />;
-                    case 'utils':
-                        return arg === 'current_datetime' ? <span key={index}>{new Date().toLocaleString()}</span> : <span key={index}>{navigator.language || navigator.userLanguage}</span>;
-                    default:
-                        return null;
+            if (part.startsWith('{icon:')) {
+                const iconName = part.substring(6, part.length - 1);
+                if (iconName === 'clock') {
+                    return (
+                        <span key={index} onClick={handleClickTime}>
+                            {icons[iconName]}
+                        </span>
+                    );
+                } else if (iconName === 'language') {
+                    return (
+                        <span key={index} onClick={handleClickLanguage}>
+                            {icons[iconName]}
+                        </span>
+                    );
                 }
+            } else if (part.startsWith('{utils:')) {
+                const utilName = part.substring(7, part.length - 1);
+                return <span key={index}>{utils[utilName]?.()}</span>;
+            } else {
+                return <span key={index}>{part}</span>;
             }
         });
     };
@@ -28,4 +50,31 @@ const TemplateRenderer: React.FC<TemplateProps> = ({ template }) => {
     return <>{renderTemplate()}</>;
 };
 
-export default TemplateRenderer;
+const App: React.FC = () => {
+    const icons: IconConfig = {
+        clock: <span role="img" aria-label="Clock" style={{ cursor: 'pointer' }}>⏰</span>,
+        language: <span role="img" aria-label="Language" style={{ cursor: 'pointer' }}>🌐</span>,
+    };
+
+    const utils: UtilConfig = {
+        current_datetime: () => new Date().toLocaleString(),
+        language: () => navigator.language || navigator.userLanguage,
+    };
+
+    return (
+        <div className="App">
+            <h1>Шаблонизатор</h1>
+            <div>
+                <p>
+                    <Template
+                        template="Current time is {icon:clock}, selected language is {icon:language}"
+                        icons={icons}
+                        utils={utils}
+                    />
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
